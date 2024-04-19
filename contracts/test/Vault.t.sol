65f2cc6c900ac71d23dca4bcae9a1e875320b661
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2024-04-19 16:58:40 -0400

    Implement vault basic tests

--- contracts/test/Vault.t.sol
+++ contracts/test/Vault.t.sol
@@ -7,21 +7,44 @@ import {Test, console2} from "forge-std/Test.sol";
 import {Vault, IEgeneLayerConstracts} from "../src/Vault.sol";
 import "./TestCoin.sol";
 import {HoldingsManager} from "../src/HoldingsManager.sol";
+import {IEgeneLayerConstracts, TestnetContracts} from "../src/EigenLayerContracts.sol";
 
 
 contract AssetManagerTest is Test {
     Vault public vault;
-    TestCoin public coin;
+    TestCoin public rewardsToken;
+    TestCoin public liquidStakedToken;
 
     function setUp() public {
         vm.startPrank(msg.sender);
-        coin = new TestCoin("AVS1 Rewards Token", "AVS1");
-        coin.mint(msg.sender, 100);
-        vault = new Vault(coin, IEgeneLayerConstracts(address(0x0)), HoldingsManager(address(0x0)));
+        
+        rewardsToken = new TestCoin("AVS1 Rewards Token", "AVS1");
+        rewardsToken.mint(msg.sender, 100);
+
+        liquidStakedToken = new TestCoin("Liquid Staked ETH", "lsETH");
+        liquidStakedToken.mint(msg.sender, 100);
+
+        IEgeneLayerConstracts elContracts = new TestnetContracts();
+        HoldingsManager holdingsManager = new HoldingsManager(address(msg.sender));
+        vault = new Vault(liquidStakedToken, elContracts, holdingsManager);
+    }
+
+    function test_ERC20UnderlyingAsset() public {
+        require(vault.asset() == address(liquidStakedToken));
+    }
+
+    function test_deposit() public {
+        liquidStakedToken.approve(address(vault), 100);
+        vault.deposit(10, msg.sender);
+        assertEq(vault.balanceOf(msg.sender), 10);
+        assertEq(liquidStakedToken.balanceOf(msg.sender), 100 - 10);
+        assertEq(vault.totalDeposited(), 10);
+    }
+
+    function test_minting() public {
+        assertEq(liquidStakedToken.balanceOf(msg.sender), 100);
+        assertEq(rewardsToken.balanceOf(msg.sender), 100);
     }
 
 
-    function test_1() public {
-        require(coin.balanceOf(msg.sender) == 100);
-    }
 }
