c9f8951aca8e62c071b5f079ad17b24ae6a96f40
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2024-04-19 10:05:57 -0400

    Update readme.md

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # EigenLayer FundManager
 
 ![](docs/SystemDesign.png)
+
+
+## Dev
+
+
+1. Install foundry
+
+```bash
+curl -L https://foundry.paradigm.xyz | bash
+```
+
+2. Pull repo and submodules `git submodule update --init --recursive`
+
+3. Go to contracts and run `forge bulid` or `forge test`
--- contracts/src/Vault.sol
+++ contracts/src/Vault.sol
@@ -6,6 +6,14 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "./HoldingsManager.sol";
 
+import "eigenlayer-contracts/src/contracts/core/DelegationManager.sol";
+import "eigenlayer-contracts/src/contracts/core/StrategyManager.sol";
+
+interface IUserDeployer {
+    function delegationManager() external view returns (DelegationManager);
+    function strategyManager() external view returns (StrategyManager);
+}
+
 contract Vault is ERC4626 {
     // Assuming HoldingsManager is defined elsewhere in your project
     HoldingsManager holdingsManager;
@@ -58,5 +66,42 @@ contract Vault is ERC4626 {
         address receiver
     ) public override returns (uint256) {
         return super.mint(shares, receiver);
+    }
+
+
+    /* Function has to be called from deposit()
+        1. Get the list of Operators and stake in the porfolio
+        2. Check out current investment positions (add vault holdings map)
+        3. redistribute available funds over available Operators to keep "stake_bps" relevant
+    */
+    function _stake() {
+
+    }
+
+
+    /* Function has to be called from withdraw()
+        1. Get the list of Operators and stake in the porfolio
+        2. Check out current investment positions (add vault holdings map)
+        3. withdraw available funds from the available Operators to keep "stake_bps" relevant
+    */
+    function _unstake() {
+
+    }
+
+    /*  Called from the _stake()
+        Interact with EigenLayer DelegationManager
+    */
+    function _delegate() {
+
+    }
+
+    /*
+        Called from _unstake()
+        Interacts with EigenLayer DelegationManager
+        Check out https://github.com/Layr-Labs/eigenlayer-contracts/blob/7229f2b426b6f2a24c7795b1a4687a010eac8ef2/src/test/integration/users/User.t.sol#L19 
+
+    */
+    function _undelegate() {
+
     }
 }
