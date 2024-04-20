4d0a7c166d97f0406830c824e0d4f99c90eb3593
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2024-04-20 17:11:25 -0400

    Update scripts

--- contracts/script/TestDeposit.sol
+++ contracts/script/TestDeposit.sol
@@ -45,14 +45,29 @@ contract TestMyOperator is Script {
     function run() public {
         IEigenLayerContracts eigenLayerContracts = IEigenLayerContracts(vm.envAddress("EIGEN_LAYER_CONTRACTS_ADDRESS"));
         ERC20 liquidStakedToken = ERC20(address(0xB4F5fc289a778B80392b86fa70A7111E5bE0F859));
-        uint256 testDeposit = 100;
 
         vm.startBroadcast();
         // Coinbase Operator: https://holesky.etherscan.io/address/0xbe4b4fa92b6767fda2c8d1db53a286834db19638
         MyOperator mOperator = new MyOperator(address(0xbE4B4Fa92b6767FDa2C8D1db53A286834dB19638));
         console2.log("MyOperator address", address(mOperator));
+        vm.stopBroadcast();
+    }
+}
+
+contract DeployMyOperator is Script {
+    function setUp() public {}
+
+    function run() public {
+        IEigenLayerContracts eigenLayerContracts = IEigenLayerContracts(vm.envAddress("EIGEN_LAYER_CONTRACTS_ADDRESS"));
+        ERC20 liquidStakedToken = ERC20(address(0xB4F5fc289a778B80392b86fa70A7111E5bE0F859));
+        MyOperator mOperator = new MyOperator(vm.envAddress("MY_OPERATOR_ADDRESS"));
+
+        vm.startBroadcast();
         liquidStakedToken.approve(address(mOperator), 10);
         mOperator.stake(liquidStakedToken, 10, eigenLayerContracts);
         vm.stopBroadcast();
     }
 }
+
+
+
